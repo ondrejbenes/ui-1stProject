45adcb7b2c9751d8120046d07b2649f14447ae63
Beny <st39703@student.upce.cz>
2015-11-08 17:12:52 +0100

    Added AStar

---
+++ 1stProject/BARIN/PathTransformer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barin
+{
+    class PathTransformer
+    {
+        public static LinkedList<AbstractAction> TransformNodePathToActionPath(LinkedList<Node> NodePath)
+        {
+            var nodePathArray = NodePath.ToArray();
+            LinkedList<AbstractAction> actionPath = new LinkedList<AbstractAction>();
+            for (int i = 0; i < NodePath.Count; i++)
+            {
+                foreach (var child in nodePathArray[i].Children)
+                {
+                    if (child.Item1.State.Equals(nodePathArray[i + 1].State))
+                        actionPath.AddLast(child.Item2);
+                }
+            }
+
+            return actionPath;
+        }
+    }
+}
---
+++ 1stProject/TilesHeuristicCalculator.cs
@@ -0,0 +1,47 @@
+﻿using Barin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1stProject
+{
+    class TilesHeuristicCalculator : HeuristicCalculator
+    {
+        override public double Calculate(AbstractState start, AbstractState goal)
+        {
+            if (!(start is TilesState) || !(goal is TilesState))
+                throw new ArgumentException("Only TileStates are allowed");
+
+            TilesState startState = (TilesState)start;
+            TilesState goalState = (TilesState)goal;
+            double heuristic = 0;
+
+            for (int i = 0; i < TilesState.rows; i++)
+            {
+                for (int j = 0; j < TilesState.columns; j++)
+                {
+                    Tuple<int, int> indexesInGoal = getIndexes(startState.GetTiles()[i, j], goalState.GetTiles());
+                    heuristic += Math.Abs(indexesInGoal.Item1 - i) + Math.Abs(indexesInGoal.Item2 - j);
+                }
+            }
+
+            return heuristic;
+        }
+
+        private Tuple<int, int> getIndexes(int tileValue, int[,] tiles)
+        {
+            for (int i = 0; i < TilesState.rows; i++)
+            {
+                for (int j = 0; j < TilesState.columns; j++)
+                {
+                    if(tiles[i, j] == tileValue)
+                        return new Tuple<int, int>(i, j);
+                }
+            }
+
+            throw new BarinException("Value " + tileValue + "not found in tilesMatrix");
+        }
+    }
+}
